7b760077bde5b0ffd8dde744a298522c16f6d10c
John Robertson <jrobertson@callware.com>
2019-08-05 14:07:53 -0600

    migrated to platform data types

--- x64/include/ltdl.h
+++ x64/include/ltdl.h
@@ -16,18 +16,23 @@ int lt_dlinit()
 const char* lt_dlerror()
 {
     DWORD dLastError = GetLastError();
-    LPCTSTR strErrorMessage = NULL;
+	LPTSTR strErrorMessage = NULL;
 
     FormatMessage(
         FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS | FORMAT_MESSAGE_ARGUMENT_ARRAY | FORMAT_MESSAGE_ALLOCATE_BUFFER,
         NULL,
         dLastError,
         0,
-        (LPWSTR) &strErrorMessage,
+        (LPTSTR)&strErrorMessage,
         0,
         NULL
     );
     return strErrorMessage;
+}
+
+void lt_dlerrorfree(const char * msg)
+{
+	if (msg) LocalFree((HLOCAL)msg);
 }
 
 lt_dlhandle lt_dlopen(char* p)
@@ -35,9 +40,9 @@ lt_dlhandle lt_dlopen(char* p)
     return LoadLibrary(p);
 }
 
-void* lt_dlsym(lt_dlhandle hndl, char* s)
+void* lt_dlsym(lt_dlhandle hndl, const char* s)
 {
-    return GetProcAddress(hndl, s);
+    return (void*)GetProcAddress(hndl, (LPCSTR)s);
 }
 
 #endif /* !LTDL_H */
